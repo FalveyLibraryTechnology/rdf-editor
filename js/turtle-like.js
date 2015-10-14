e0c9e946c8ea624599c9214a66eccaae417af2e8
Chris Hallberg <crhallberg@gmail.com>
2015-10-14 13:23:42 -0400

    dc and owl

--- js/turtle-like.js
+++ js/turtle-like.js
@@ -7,17 +7,20 @@ function init() {
   tabOverride.autoIndent(false);
   tabOverride.tabSize(4);
   loadAll([
+    './authorities/dc.json',
     './authorities/foaf.json',
-    './authorities/xsd.json',
-    './authorities/schema.org.json',
+    './authorities/owl.json',
     './authorities/rdf.json',
     './authorities/rdfschema.json',
+    './authorities/schema.org.json',
+    './authorities/xsd.json',
   ]).then(parseJSONs);
 }
 
 function parseJSONs(jsons) {
   var classes = [];
   var predicates = [];
+  var datatypes = [];
   for (var i = 0; i < jsons.length; i++) {
     console.log(jsons[i].namespace);
     for (var j = 0; j < jsons[i].classes.length; j++) {
@@ -26,9 +29,14 @@ function parseJSONs(jsons) {
     for (var j = 0; j < jsons[i].predicates.length; j++) {
       predicates.push(jsons[i].namespace + ':' + jsons[i].predicates[j].attr);
     }
+    if (!!jsons[i].datatypes) {
+      for (var j = 0; j < jsons[i].datatypes.length; j++) {
+        datatypes.push(jsons[i].namespace + ':' + jsons[i].datatypes[j].attr);
+      }
+    }
   }
   editor.textcomplete([
-    { // tech companies
+    { // predicates
       words: predicates,
       match: /\n[ ]{4}\b([\w\:]{2,})$/,
       search: function (term, callback) {
@@ -41,17 +49,18 @@ function parseJSONs(jsons) {
         return '\n    ' + word + '\n        ';
       }
     },
-    { // tech companies
+    { // rdf:type
       words: classes,
-      match: /\n[ ]{8}\b([\w\:]{2,})$/,
+      match: /\n[ ]{4}rdf:type\n[ ]{8}\b([\w\:]*)$/,
       search: function (term, callback) {
+        var regex = new RegExp(term, 'i');
         callback($.map(this.words, function (word) {
-          return word.indexOf(term) > -1 ? word : null;
+          return word.match(regex) ? word : null;
         }));
       },
       index: 1,
       replace: function (word) {
-        return '\n        ' + word + '\n        ';
+        return '\n    rdf:type\n        ' + word + '\n    ';
       }
     }
   ]);
