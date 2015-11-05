ff518083608c70bc9a345ad65f7aec17013a6d32
Chris Hallberg <crhallberg@gmail.com>
2015-11-05 10:54:49 -0500

    Languages and changes

--- js/column-by-div.js
+++ js/column-by-div.js
@@ -1,19 +1,16 @@
+var tree, current = {
+  subject: false,
+  predicate: false
+};
 function addNewTerm(type) {
-  if (typeof type === 'undefined') {
-    type = 'predicate';
-  }
-  if (editor.terms.length > 0) {
-    console.log(editor.terms[editor.terms.length-1].value);
-    if (editor.terms[editor.terms.length-1].value === 'rdf:type') {
-      type = 'type';
-    }
-  }
-  var div = document.createElement('div');
-  div.className = type;
   var input = document.createElement('input');
   input.type = 'text';
-  input.index = editor.terms.length;
   input.onkeydown = control;
+  input.onkeyup = contentControl;
+  input.onfocus = function() {
+    editor.focused = this.index;
+    updateCurrents();
+  }
   $(input).autocomplete({
     maxResults: 10,
     loadingString: 'Loading...',
@@ -21,19 +18,39 @@ function addNewTerm(type) {
       cb(autocomplete(input, query));
     }
   });
+  var div = document.createElement('div');
+  div.className = type;
+  if (typeof type === 'undefined') {
+    type = 'predicate';
+  }
   div.appendChild(input);
-  editor.appendChild(div);
-  editor.terms.push(input);
+  if (typeof editor.focused === 'undefined') {
+    editor.focused = -1;
+  }
+  input.index = editor.focused+1;
+  if (input.index == editor.terms.length) {
+    editor.appendChild(div);
+    editor.terms.push(input);
+  } else {
+    $(div).insertAfter(editor.terms[editor.focused].parentNode);
+    editor.terms.splice(editor.input, 0, input);
+    for (var i=0; i<editor.terms.length; i++) {
+      editor.terms[i].index = i;
+    }
+  }
   input.focus();
   return input;
 }
 
 function autocomplete(op, query) {
-  if ('undefined' === typeof completeSets[op.parentNode.className]) {
+  var set = current.predicate === 'rdf:type'
+    ? 'type'
+    : op.parentNode.className
+  if ('undefined' === typeof completeSets[set]) {
     return [];
   }
-  return $.map(
-    completeSets[op.parentNode.className],
+  var items = $.map(
+    completeSets[set],
     function (word) {
       var regex = new RegExp(query, 'i');
       if (word.label.match(regex)) {
@@ -49,6 +66,28 @@ function autocomplete(op, query) {
       return null;
     }
   );
+  if (items.length < 10) {
+    var dmatches = $.map(
+      completeSets[set],
+      function (word) {
+        if (typeof word.description !== 'undefined') {
+          var regex = new RegExp(query, 'i');
+          if (word.description.match(regex)) {
+            return {
+              val: word.label,
+              description: word.description
+            }
+          }
+          return null;
+        }
+      }
+    );
+    var i = 0;
+    while(items.length < 10 && i < dmatches.length) {
+      items.push(dmatches[i++]);
+    }
+  }
+  return items;
 }
 
 var indent = {
@@ -69,6 +108,7 @@ function control(e) {
     e.preventDefault();
     if (empty) {
       this.parentNode.className = outdent[this.parentNode.className];
+      updateCurrents();
     } else {
       var n = this.parentNode.className === 'predicate'
         ? 'object'
@@ -90,9 +130,11 @@ function control(e) {
       this.parentNode.className = e.shiftKey
         ? outdent[this.parentNode.className]
         : indent[this.parentNode.className];
+      updateCurrents();
     } else if (8 === e.which) { // BACKSPACE
       this.parentNode.className = outdent[this.parentNode.className];
-    } else if (46 === e.which) {
+      updateCurrents();
+    } else if (46 === e.which) { // DELETE
       var oldIndex = this.index;
       editor.terms.splice(this.index, 1);
       for (var i=0; i<editor.terms.length; i++) {
@@ -101,6 +143,41 @@ function control(e) {
       editor.removeChild(this.parentNode);
       editor.terms[Math.min(oldIndex, editor.terms.length-1)].focus();
     }
+  }
+}
+function contentControl() {
+  if (
+    (this.parentElement.className === 'object'
+    || this.parentElement.className === 'lang')
+    && this.value.match(/\^\^/)
+  ) {
+    this.value = this.value.substr(0, this.value.length-2);
+    var it = addNewTerm('type');
+    it.value = 'xsd:';
+  } else if (
+    (this.parentElement.className === 'object'
+    || this.parentElement.className === 'type')
+    && this.value.match(/\@/)
+  ) {
+    this.value = this.value.substr(0, this.value.length-2);
+    var it = addNewTerm('lang');
+    it.value = 'lang:';
+  }
+}
+
+function updateCurrents() {
+  current.subject = false;
+  current.predicate = false;
+  for (var i=editor.focused+1; i-- && (!current.subject || !current.predicate); ) {
+    if (false === current.subject && editor.terms[i].parentNode.className === 'subject') {
+      current.subject = editor.terms[i].value;
+    }
+    if (false === current.predicate && editor.terms[i].parentNode.className === 'predicate') {
+      current.predicate = editor.terms[i].value;
+    }
+  }
+  if (current.subject.length == 0) {
+    current.predicate = false;
   }
 }
 
@@ -112,6 +189,7 @@ function init() {
   loadAll([
     './authorities/dc.json',
     './authorities/foaf.json',
+    './authorities/languages.json',
     './authorities/owl.json',
     './authorities/rdf.json',
     './authorities/rdfschema.json',
@@ -123,24 +201,28 @@ function init() {
 var completeSets = {
   predicate: [],
   type: [],
-  datatype: []
+  datatype: [],
+  lang: []
 };
 function parseJSONs(jsons) {
   for (var i = 0; i < jsons.length; i++) {
-    // console.log(jsons[i].namespace);
-    for (var j = 0; j < jsons[i].classes.length; j++) {
-      var item = { label: jsons[i].namespace + ':' + jsons[i].classes[j].attr };
-      if (typeof jsons[i].classes[j].description !== 'undefined') {
-        item.description = jsons[i].classes[j].description;
+    if (!!jsons[i].classes) {
+      for (var j = 0; j < jsons[i].classes.length; j++) {
+        var item = { label: jsons[i].namespace + ':' + jsons[i].classes[j].attr };
+        if (typeof jsons[i].classes[j].description !== 'undefined') {
+          item.description = jsons[i].classes[j].description;
+        }
+        completeSets.type.push(item);
       }
-      completeSets.type.push(item);
     }
-    for (var j = 0; j < jsons[i].predicates.length; j++) {
-      var item = { label: jsons[i].namespace + ':' + jsons[i].predicates[j].attr };
-      if (typeof jsons[i].predicates[j].description !== 'undefined') {
-        item.description = jsons[i].predicates[j].description;
+    if (!!jsons[i].predicates) {
+      for (var j = 0; j < jsons[i].predicates.length; j++) {
+        var item = { label: jsons[i].namespace + ':' + jsons[i].predicates[j].attr };
+        if (typeof jsons[i].predicates[j].description !== 'undefined') {
+          item.description = jsons[i].predicates[j].description;
+        }
+        completeSets.predicate.push(item);
       }
-      completeSets.predicate.push(item);
     }
     if (!!jsons[i].datatypes) {
       for (var j = 0; j < jsons[i].datatypes.length; j++) {
@@ -149,6 +231,15 @@ function parseJSONs(jsons) {
           item.description = jsons[i].datatypes[j].description;
         }
         completeSets.datatype.push(item);
+      }
+    }
+    if (!!jsons[i].languages) {
+      for (var j = 0; j < jsons[i].languages.length; j++) {
+        var item = { label: jsons[i].namespace + ':' + jsons[i].languages[j].attr };
+        if (typeof jsons[i].languages[j].description !== 'undefined') {
+          item.description = jsons[i].languages[j].description;
+        }
+        completeSets.lang.push(item);
       }
     }
   }
